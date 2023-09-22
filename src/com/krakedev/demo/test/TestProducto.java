d8a3e7261f5d91b03218c4395c322ed03605abf2
santiagoMosquera99 <>
2023-09-22 12:30:32 -0500

    Demo

---
+++ src/com/krakedev/demo/test/TestProducto.java
@@ -0,0 +1,15 @@
+package com.krakedev.demo.test;
+
+import com.krakedev.demo.Producto;
+
+public class TestProducto {
+
+	public static void main(String[] args) {
+		Producto producto1 = new Producto(001, "Pan");
+
+		producto1.setDescripcion("Pan Blanco Bimbo Mediano 460g");
+		producto1.setPeso(460);
+		
+			producto1.imprimir();	}
+
+}
